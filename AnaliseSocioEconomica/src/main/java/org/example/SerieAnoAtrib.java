10c4c6662a52425e1c8a2591ae5cf8e101253738
vfschiavon <vischijau@gmail.com>
2023-09-28 23:59:07 -0300

    Alterações relacionadas à adição das classes de entidade

--- AnaliseSocioEconomica/src/main/java/org/example/SerieAnoAtrib.java
+++ AnaliseSocioEconomica/src/main/java/org/example/SerieAnoAtrib.java
@@ -8,18 +8,17 @@ import java.util.HashMap;
 import java.util.Map;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SerieAnoAtrib {
-    @JsonAlias({"-", "1990", "1990-1995", "1995", "1995-2000", "1999-2001", "2000", "2000-2002", "2000-2005", "2001", "2001-2003", "2002", "2002-2004", "2003", "2003-2005", "2004", "2004-2006", "2005", "2005-2007", "2005-2010", "2006", "2006-2008", "2007", "2007-2009", "2008", "2008-2010", "2009", "2009-2011", "2010", "2010-2012", "2010-2015", "2011", "2011-2013", "2012", "2012-2014", "2013", "2013-2015", "2014", "2014-2016", "2015", "2015-2017", "2015-2020", "2016", "2016-2018", "2017", "2017-2019", "2018", "2018-2020", "2019", "2020", "2021", "2022"})
-    private Map<String, String> duplaAnoAtributo = new HashMap<>();
+    private Map<Integer, String> duplaAnoAtributo = new HashMap<>();
 
-    public SerieAnoAtrib(String ano, String atributo) {
+    public SerieAnoAtrib(int ano, String atributo) {
         this.duplaAnoAtributo.put(ano, atributo);
     }
 
-    public Map<String, String> getDuplaAnoAtributo() {
+    public Map<Integer, String> getDuplaAnoAtributo() {
         return duplaAnoAtributo;
     }
 
-    public void setDuplaAnoAtributo(Map<String, String> value) {
+    public void setDuplaAnoAtributo(Map<Integer, String> value) {
         this.duplaAnoAtributo = value;
     }
 }
