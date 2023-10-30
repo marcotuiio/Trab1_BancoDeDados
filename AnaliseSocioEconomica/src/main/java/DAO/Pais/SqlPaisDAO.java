fd75a8b502410407100d7fd86045b3f9392041d3
vfschiavon <vischijau@gmail.com>
2023-10-29 21:01:02 -0300

    Rascunho, depis de uma reunião, de como ficara a ideia das DAOs

--- AnaliseSocioEconomica/src/main/java/DAO/Pais/SqlPaisDAO.java
+++ AnaliseSocioEconomica/src/main/java/DAO/Pais/SqlPaisDAO.java
@@ -1,10 +1,15 @@
 package DAO.Pais;
 
+import Controller.Controller;
 import Model.Pais;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SqlPaisDAO implements PaisDAO {
     private final Connection connection;
@@ -22,9 +27,29 @@ public class SqlPaisDAO implements PaisDAO {
     }
 
     @Override
-    public Pais read(Integer id) throws SQLException {
-        // VINICIUUSSSS FAZER GET DO PAIS AQUI
-        return null;
+    public Pais read(String siglaPais) throws SQLException {
+        Pais pais = new Pais(siglaPais);
+//        Fazer sqlDadosDAO usando switch case para montar o texto de consulta (concatenando com "+") e switch case dentro do if (result.next())
+        try (PreparedStatement statement = connection.prepareStatement(READ_QUERY)) {
+            statement.setString(1, siglaPais);
+            try (ResultSet result = statement.executeQuery()) {
+                if (result.next()) {
+                    pais.setNome(result.getString("nome_extenso"));
+                } else {
+                    throw new SQLException("Erro ao visualizar: País não encontrado.");
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PgUserDAO.class.getName()).log(Level.SEVERE, "DAO", ex);
+
+            if (ex.getMessage().equals("Erro ao visualizar: País não encontrado.")) {
+                throw ex;
+            } else {
+                throw new SQLException("Erro ao visualizar País.");
+            }
+        }
+
+        return pais;
     }
 
     @Override
