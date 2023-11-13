b42912c1ab9235ab2ea0628944efea129b172166
vfschiavon <vischijau@gmail.com>
2023-11-12 22:34:45 -0300

    Alteração nos nomes das tabelas e nos atributos 'valor'

--- script_t1bd.sql
+++ script_t1bd.sql
@@ -9,10 +9,10 @@ create table T1BD.pais (
 );
 
 
-create table T1BD.t_pib_total (
+create table T1BD.pib_total (
 	ano int,
 	sigla char(2),
-	pib_total_valor numeric(20, 2),
+	valor numeric(20, 2),
 	
 	constraint pk_pib_total primary key(ano, sigla),
 	constraint fk_pib_total foreign key(sigla) references T1BD.pais(sigla) on delete cascade,
@@ -20,10 +20,10 @@ create table T1BD.t_pib_total (
 );
 
 
-create table T1BD.t_pib_per_capita (
+create table T1BD.pib_per_capita (
 	ano int,
 	sigla char(2),
-	pib_per_capita_valor numeric(8, 2),
+	valor numeric(8, 2),
 	
 	constraint pk_pib_per_capita primary key(ano, sigla),
 	constraint fk_pib_per_capita foreign key(sigla) references T1BD.pais(sigla) on delete cascade,
@@ -31,10 +31,10 @@ create table T1BD.t_pib_per_capita (
 );
 
 
-create table T1BD.t_total_exportacao (
+create table T1BD.total_exportacao (
 	ano int,
 	sigla char(2),
-	total_exportacao_valor numeric(20, 2),
+	valor numeric(20, 2),
 	
 	constraint pk_total_exportacao primary key(ano, sigla),
 	constraint fk_total_exportacao foreign key(sigla) references T1BD.pais(sigla) on delete cascade,
@@ -42,10 +42,10 @@ create table T1BD.t_total_exportacao (
 );
 
 
-create table T1BD.t_total_importacao (
+create table T1BD.total_importacao (
 	ano int,
 	sigla char(2),
-	total_importacao_valor numeric(20, 2),
+	valor numeric(20, 2),
 	
 	constraint pk_total_importacao primary key(ano, sigla),
 	constraint fk_total_importacao foreign key(sigla) references T1BD.pais(sigla) on delete cascade,
@@ -53,10 +53,10 @@ create table T1BD.t_total_importacao (
 );
 
 
-create table T1BD.t_invest_pesq_desenv (
+create table T1BD.invest_pesq_desenv (
 	ano int,
 	sigla char(2),
-	invest_pesq_desenv_valor numeric(4, 3),
+	valor numeric(4, 3),
 	
 	constraint pk_invest_pesq_desenv primary key(ano, sigla),
 	constraint fk_invest_pesq_desenv foreign key(sigla) references T1BD.pais(sigla) on delete cascade,
@@ -67,7 +67,7 @@ create table T1BD.t_invest_pesq_desenv (
 create table T1BD.t_indiv_aces_net (
 	ano int,
 	sigla char(2),
-	indiv_aces_net_valor numeric(5, 2),
+	valor numeric(5, 2),
 	
 	constraint pk_indiv_aces_net primary key(ano, sigla),
 	constraint fk_indiv_aces_net foreign key(sigla) references T1BD.pais(sigla) on delete cascade,
@@ -75,10 +75,10 @@ create table T1BD.t_indiv_aces_net (
 );
 
 
-create table T1BD.t_idh (
+create table T1BD.idh (
 	ano int,
 	sigla char(2),
-	idh_valor numeric(4, 3),
+	valor numeric(4, 3),
 	
 	constraint pk_idh primary key(ano, sigla),
 	constraint fk_idh foreign key(sigla) references T1BD.pais(sigla) on delete cascade,
@@ -86,10 +86,10 @@ create table T1BD.t_idh (
 );
 
 
-create table T1BD.t_imp_com_inter (
+create table T1BD.imp_com_inter (
 	ano int,
 	sigla char(2),
-	imp_com_inter_valor numeric(5, 3),
+	valor numeric(5, 3),
 	
 	constraint pk_imp_com_inter primary key(ano, sigla),
 	constraint fk_imp_com_inter foreign key(sigla) references T1BD.pais(sigla) on delete cascade,
@@ -97,10 +97,10 @@ create table T1BD.t_imp_com_inter (
 );
 
 
-create table T1BD.t_imp_exportacao (
+create table T1BD.imp_exportacao (
 	ano int,
 	sigla char(2),
-	imp_exportacao_valor numeric(20, 2),
+	valor numeric(20, 2),
     -- imp_exportacao_valor numeric(6, 5), mudar pra isso?
 	
 	constraint pk_imp_exportacao primary key(ano, sigla),
@@ -109,10 +109,10 @@ create table T1BD.t_imp_exportacao (
 );
 
 
-create table T1BD.t_imp_receita_fiscal (
+create table T1BD.imp_receita_fiscal (
 	ano int,
 	sigla char(2),
-	imp_receita_fiscal_valor numeric(20, 2),
+	valor numeric(20, 2),
 	
 	constraint pk_imp_receita_fiscal primary key(ano, sigla),
 	constraint fk_imp_receita_fiscal foreign key(sigla) references T1BD.pais(sigla) on delete cascade,
@@ -120,10 +120,10 @@ create table T1BD.t_imp_receita_fiscal (
 );
 
 
-create table T1BD.t_imp_alfan_import (
+create table T1BD.imp_alfan_import (
 	ano int,
 	sigla char(2),
-	imp_alfan_import_valor numeric(20, 2),
+	valor numeric(20, 2),
 	
 	constraint pk_imp_alfan_import primary key(ano, sigla),
 	constraint fk_imp_alfan_import foreign key(sigla) references T1BD.pais(sigla) on delete cascade,
@@ -131,10 +131,10 @@ create table T1BD.t_imp_alfan_import (
 );
 
 
-create table T1BD.t_imp_renda (
+create table T1BD.imp_renda (
 	ano int,
 	sigla char(2),
-	imp_renda_valor numeric(5, 2),
+	valor numeric(5, 2),
 	
 	constraint pk_imp_renda primary key(ano, sigla),
 	constraint fk_imp_renda foreign key(sigla) references T1BD.pais(sigla) on delete cascade,
@@ -143,30 +143,30 @@ create table T1BD.t_imp_renda (
 
 use t1bd;
 select * from pais;
-select * from t_idh where sigla = 'BR';
-select * from t_imp_alfan_import where sigla = 'BR';
-select * from t_imp_com_inter where sigla = 'BR';
-select * from t_imp_receita_fiscal where sigla = 'BR';
-select * from t_imp_exportacao where sigla = 'BR';
-select * from t_imp_renda where sigla = 'BR';
-select * from t_indiv_aces_net where sigla = 'BR';
-select * from t_pib_total where sigla = 'BR';
-select * from t_pib_per_capita where sigla = 'BR';
-select * from t_invest_pesq_desenv where sigla = 'BR';
-select * from t_total_exportacao where sigla = 'BR';
-select * from t_total_importacao where sigla = 'BR';
+select * from idh where sigla = 'BR';
+select * from imp_alfan_import where sigla = 'BR';
+select * from imp_com_inter where sigla = 'BR';
+select * from imp_receita_fiscal where sigla = 'BR';
+select * from imp_exportacao where sigla = 'BR';
+select * from imp_renda where sigla = 'BR';
+select * from indiv_aces_net where sigla = 'BR';
+select * from pib_total where sigla = 'BR';
+select * from pib_per_capita where sigla = 'BR';
+select * from invest_pesq_desenv where sigla = 'BR';
+select * from total_exportacao where sigla = 'BR';
+select * from total_importacao where sigla = 'BR';
 
 -- -- DROPs
 drop table pais;
-drop table t_idh;
-drop table t_imp_alfan_import;
-drop table t_imp_com_inter;
-drop table t_imp_receita_fiscal;
-drop table t_imp_exportacao;
-drop table t_imp_renda;
-drop table t_indiv_aces_net;
-drop table t_pib_total;
-drop table t_pib_per_capita;
-drop table t_invest_pesq_desenv;
-drop table t_total_exportacao;
-drop table t_total_importacao;
+drop table idh;
+drop table imp_alfan_import;
+drop table imp_com_inter;
+drop table imp_receita_fiscal;
+drop table imp_exportacao;
+drop table imp_renda;
+drop table indiv_aces_net;
+drop table pib_total;
+drop table pib_per_capita;
+drop table invest_pesq_desenv;
+drop table total_exportacao;
+drop table total_importacao;
